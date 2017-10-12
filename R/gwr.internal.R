6d66aac2db746c2a6e04ffd991c4a25cd1667e17
vpellissier <vincent.pellissier@gmail.com>
2017-10-12 12:54:33 +0200

    Fixing gwr.internal again (returning a different matrix depending on whether se.fit=TRUE or FALSE)

--- R/gwr.internal.R
+++ R/gwr.internal.R
@@ -48,14 +48,16 @@ gwr.internal<-function(x, y, cell, coords, bandwidth, weights=NULL,
 
     }
 
-    if(se.fit==TRUE) 
+    if(se.fit==TRUE){
         coeffs.se.i<-diag(invZ)
-    else
-        coeffs.se.i<-NA
-
-    df.i<-c(sum.weights, coeffs.i, coeffs.se.i, pred.i, gwr.e.i)
-    names(df.i)<-c("sum.weights", names(coeffs.i), 
+        df.i<-c(sum.weights, coeffs.i, coeffs.se.i, pred.i, gwr.e.i)
+        names(df.i)<-c("sum.weights", names(coeffs.i), 
                     paste0("SE_", names(coeffs.i)), "yhat", "gwr.error")
+    }
+    else{
+        df.i<-c(sum.weights, coeffs.i, pred.i, gwr.e.i)
+        names(df.i)<-c("sum.weights", names(coeffs.i), "yhat", "gwr.error")
+    }
     
     return(list(df.i=df.i, lhat.i=lhat.i))
 }
